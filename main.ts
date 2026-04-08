af4632c1dcfd126a71d2bc51a9c89748992edfb9
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-04-08 00:28:02 -0400

    Servo

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Illia
+ * Created on: Apr 2026
+ * This program Servo
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// direction (0°)
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 0)
+    basic.showIcon(IconNames.Happy)
+})
+
+// direction (180°)
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+
+    basic.showIcon(IconNames.Happy)
+})
